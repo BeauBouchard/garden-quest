62b9063d99d34bbb3b6845f54a7e53de02b18f41
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-06-07 12:49:01 -0400

    feat: create player.h

---
+++ scratch/player.h
@@ -0,0 +1,29 @@
+/*
+
+ PLAYER.H
+
+ Include File.
+
+ Info:
+  Form                 : All tiles as one unit.
+  Format               : Gameboy 4 color.
+  Compression          : None.
+  Counter              : None.
+  Tile size            : 8 x 8
+  Tiles                : 0 to 0
+
+  Palette colors       : None.
+  SGB Palette          : None.
+  CGB Palette          : None.
+
+  Convert to metatiles : No.
+
+*/
+
+
+/* Bank of tiles. */
+#define playerBank 0
+/* Start of tile array. */
+extern unsigned char player[];
+
+/* End of PLAYER.H */
